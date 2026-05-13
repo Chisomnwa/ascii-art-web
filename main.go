f979ed409bf51d920a498f37f2f3c9655d22eb1f
ogajayi <ajaioghenekevwe@gmail.com>
2026-05-13 12:19:44 +0100

    feat:update the functions and folder structure

--- main.go
+++ main.go
@@ -1,54 +1,18 @@
 package main
 
 import (
+	asciiartcode "ascii-art-web/ascii-art-code"
+	"ascii-art-web/handlers"
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
-type TemplateData struct {
-	Result string
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		// w.Write([]byte("page not found"))
-		http.NotFound(w, r)
-		return
-	}
-	temp, _ := template.ParseFiles("./templates/index.html")
-	temp.Execute(w, nil)
-
-}
-
-func AsciiArtHandler(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-
-	fmt.Println("In ascii art handler", r.Form)
-	fmt.Println("Expected post", r.PostForm)
-
-	inputText := r.Form.Get("inputText")
-	banner := r.Form.Get("banner")
-
-	result := asciiArt(inputText, banner)
-
-	fmt.Println(result)
-
-	tmpl, err := template.ParseFiles("./templates/index.html")
-	if err != nil {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	tmpl.Execute(w, &TemplateData{Result: result})
-}
-
 func main() {
 
-	http.HandleFunc("GET /", rootHandler)
-	http.HandleFunc("POST /ascii-art", AsciiArtHandler)
+	http.HandleFunc("GET /", handlers.RootHandler)
+	http.HandleFunc("POST /ascii-art", handlers.AsciiArtHandler)
 
-	startRes := asciiArt("Our Server", "standard")
+	startRes, _ := asciiartcode.AsciiArt("Our Server", "standard")
 	fmt.Print(startRes)
 	fmt.Println()
 
